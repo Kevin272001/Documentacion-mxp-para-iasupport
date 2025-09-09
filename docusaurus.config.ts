1f1bef5d525ec4d6a7b28f6b28516e488642a436
kevin272001 <kevin27carvajalg@gmail.com>
2025-09-08 23:50:10 -0500

    creacion carpetas

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
-  future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
-  },
-
+ 
   // Set the production url of your site here
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
